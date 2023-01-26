5d7f93942e92ea7d7aa995e37ce32fa13b272996
Tommy <tf01@fastmail.com>
2023-01-26 21:16:05 +1000

    final results of the day

--- specter/analyse.py
+++ specter/analyse.py
@@ -7,14 +7,16 @@ import matplotlib.pyplot as plt
 # %% get data
 # https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
 data = p.read_csv('specter.csv', index_col="Song")
+data.head(20)
 
+# %%
+# drop if required
 # dropping = ["Saph"]
 # data = data.drop(labels=dropping, axis='columns')
 # %% Describe the data
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html
 p.set_option('display.float_format', lambda x: '%.2f' % x)
-output = data.describe().to_string()
-print(output)
+data.describe()
 
 # %% for info per song
 data_songs = data.transpose()
