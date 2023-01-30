6996aaf1d1db908a6a65f6b4786a3e1ebbaab39e
Tommy <tf01@fastmail.com>
2023-01-30 20:23:25 +1000

    changes and initial html generation

--- specter/analyse.py
+++ specter/analyse.py
@@ -3,10 +3,20 @@ import numpy as np
 import pandas as p
 import matplotlib.pyplot as plt
 
+# configure
+p.set_option('display.float_format', lambda x: '%.2f' % x)
+
+# simple save to file
+
+def write_to_file(filename, contents):
+    file = open(filename, 'w')
+    file.write(contents)
+    file.close()
+
 # %% get data
 # https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
 data = p.read_csv('specter.csv', index_col="Song")
-data.head(20)
+write_to_file('raw-data.html', data.head(20).to_html())
 
 # %%
 # drop if required
@@ -14,20 +24,18 @@ data.head(20)
 # data = data.drop(labels=dropping, axis='columns')
 # %% Describe the data
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.describe.html
-p.set_option('display.float_format', lambda x: '%.2f' % x)
 data_description = data.describe()
-data.describe()
+write_to_file('data-desc.html', data_description.to_html())
 
 # %% for info per song
 data_songs = data.transpose()
 data_songs_description = data_songs.describe()
-data_songs.describe()
+write_to_file('data-songs-desc.html', data_songs_description.to_html())
 
 # %% for info on entire album, collapse into a single column
-data_stack = data.stack(dropna=True)
-data_stack.describe()
+data_stack = data.stack(dropna=True).to_frame()
+write_to_file('data-all.html', data_stack.describe().to_html())
 
-# %% extract means for boxplots
 # %% histograms
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.hist.html
 ax = data.hist(figsize=(20, 20), sharey=True, sharex=True)
@@ -39,7 +47,7 @@ ax[0][0].figure.savefig('hist-songs.png')
 
 # %% histogram of everything
 ax = data_stack.hist(figsize=(10, 10))
-ax.figure.savefig('hist-all.png')
+ax[0][0].figure.savefig('hist-all.png')
 
 # %% boxplots
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.boxplot.html#matplotlib.pyplot.boxplot
@@ -67,7 +75,7 @@ ax.set_xlabel('Songs')
 ax.figure.savefig('box-songs.png')
 
 # %% stacked data boxplot
-ax = data_stack.to_frame().boxplot(figsize=(15, 15), return_type='axes', rot=45, showmeans=True)
+ax = data_stack.boxplot(figsize=(15, 15), return_type='axes', rot=45, showmeans=True)
 ax.set_title('RC Server \'Specter\' Entire Dataset Boxplot')
 ax.set_ylabel('Score')
 ax.set_xlabel('Dataset')
